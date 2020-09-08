524d93dcd1a6fe26e8429a107fe58aa67ee9d828
Mat Kelcey <matthew.kelcey@gmail.com>
2020-09-09 09:46:43 +1000

    test against both validation and test set

--- test.py
+++ test.py
@@ -10,14 +10,10 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--num-models', type=int, default=1)
 parser.add_argument('--dense-kernel-size', type=int, default=16)
 parser.add_argument('--saved-model', type=str, required=True)
-parser.add_argument('--split', type=str, default='test')
 opts = parser.parse_args()
-print(opts, file=sys.stderr)
 
-if opts.split not in ['validate', 'test']:
-    raise Exception()
-
-# restore model
+# construct model
+# TODO: would be better to be able to load from save without this config
 if opts.num_models == 1:
     net = models.NonEnsembleNet(num_classes=10,
                                 dense_kernel_size=opts.dense_kernel_size,
@@ -27,14 +23,16 @@ else:
                              num_classes=10,
                              dense_kernel_size=opts.dense_kernel_size,
                              seed=0)
+
+# restore from save
 objax.io.load_var_collection(opts.saved_model, net.vars())
 
-# read entire dataset
-if opts.split == 'validate':
-    imgs, labels = data.validation_dataset()
-else:
-    imgs, labels = data.test_dataset()
+# check against validation set
+imgs, labels = data.validation_dataset()
+accuracy = util.accuracy(net.predict(imgs, single_result=True), labels)
+print("validation accuracy %0.3f" % accuracy)
 
-# final validation metrics
-accuracy = util.accuracy(net.predict(imgs), labels)
-print("accuracy", opts.split, accuracy)
+# check against test set
+imgs, labels = data.test_dataset()
+accuracy = util.accuracy(net.predict(imgs, single_result=True), labels)
+print("test accuracy %0.3f" % accuracy)
