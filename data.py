f630271e69b2ea91a90e348dd1d202f76a6c7507
Mat Kelcey <matthew.kelcey@gmail.com>
2020-09-12 11:24:06 +1000

    have data generate collages of each class

--- data.py
+++ data.py
@@ -73,12 +73,40 @@ if __name__ == '__main__':
     def pil_img_from_array(array):
         return Image.fromarray((array * 255.0).astype(np.uint8))
 
-    B = 4
-    for imgs, labels in training_dataset(batch_size=B*B,
-                                         num_inputs=3):
-        break
-    collage = Image.new('RGB', (64*B, 64*B))
-    for i in range(B*B):
-        r, c = i//B, i % B
-        collage.paste(pil_img_from_array(imgs[0, i]), (r*64, c*64))
-    collage.resize((64*B*3, 64*B*3)).show()
+    SAMPLE_GRID_SIZE = 5   # each sample will have SGSxSGS images
+    NUM_CLASSES = 10
+
+    class Collage(object):
+        def __init__(self):
+            self.collage = Image.new('RGB', (64*SAMPLE_GRID_SIZE,
+                                             64*SAMPLE_GRID_SIZE))
+            self.insert_idx = 0
+            self.full = False
+
+        def add(self, np_array):
+            # return true on last pasted
+            if self.full:
+                return False
+            x = int((self.insert_idx // SAMPLE_GRID_SIZE) * 64)
+            y = int((self.insert_idx % SAMPLE_GRID_SIZE) * 64)
+            self.collage.paste(pil_img_from_array(np_array), (x, y))
+            self.insert_idx += 1
+            if self.insert_idx == SAMPLE_GRID_SIZE * SAMPLE_GRID_SIZE:
+                self.full = True
+            return self.full
+
+    collages = [Collage() for _ in range(NUM_CLASSES)]
+
+    num_collages_full = 0
+    for imgs, labels in training_dataset(batch_size=128):
+        for img, label in zip(imgs, labels):
+            if collages[label].add(img):
+                num_collages_full += 1
+                if num_collages_full == 10:
+                    break
+
+    class_names = ['Annual_Crop', 'Forest', 'Herbaceous_Vegetation', 'Highway',
+                   'Industrial_Buildings', 'Pasture', 'Permanent_Crop',
+                   'Residential_Buildings', 'River', 'Sea_Lake']
+    for i, c in enumerate(collages):
+        c.collage.save("collage.%02d.%s.png" % (i, class_names[i]))
